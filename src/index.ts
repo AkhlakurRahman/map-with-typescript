2107ed9e416b94afb712882e1d568fad422e2b3d
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2020-01-13 03:07:27 +0600

    google map is showing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hi Thre');
+// import { Company } from './Company';
+// import { User } from './User';
+
+// const mapDiv = document.getElementById('map');
+
+new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
